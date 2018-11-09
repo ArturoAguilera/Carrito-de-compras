4d7608064a032c4ebd94c56bbd6ac1d80112a802
ArturoAguilera <44881764+ArturoAguilera@users.noreply.github.com>
2018-11-09 16:48:01 -0600

    Create Carrito.java

---
+++ Carrito.java
@@ -0,0 +1,12 @@
+package poo.proyectofinal.carritodecompras.elementos;
+
+import java.util.ArrayList;
+
+public class Carrito{
+    private ArrayList<ArrayList<Producto>> producto;
+    
+    public void agregarProducto(Producto producto){
+    
+    }
+    
+}
